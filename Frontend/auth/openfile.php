1c381605c10d9469debb99c0ed0ae5f2d8b54eec
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-15 19:09:21 +0200

    Update openfile.php

--- Frontend/auth/openfile.php
+++ Frontend/auth/openfile.php
@@ -1,4 +1,4 @@
 <?php
 // zurubank/Frontend/auth/openfile.php
 
-require_once __DIR__ . '/../../Backend/api/v1/verify_asset.php';
+require_once __DIR__ . '/../../Backend/helpers/CertificateManager.php';
