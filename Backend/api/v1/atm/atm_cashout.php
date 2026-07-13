db84cd9646977d9e5ba65f0ddba798f0b79dee5a
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-13 07:05:10 +0200

    Update atm_cashout.php

--- Backend/api/v1/atm/atm_cashout.php
+++ Backend/api/v1/atm/atm_cashout.php
@@ -9,10 +9,10 @@ header('Content-Type: application/json');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/../../../config/db.php';
-require __DIR__ . '/../../../helpers/response.php';
-require __DIR__ . '/../../../helpers/crypto.php';
-require __DIR__ . '/../../../helpers/CertificateManager.php';
+require_once __DIR__ . '/../../../config/db.php';
+require_once __DIR__ . '/../../../helpers/response.php';
+require_once __DIR__ . '/../../../helpers/crypto.php';
+require_once __DIR__ . '/../../../helpers/CertificateManager.php';
 
 // -------------------------
 // Read Input
